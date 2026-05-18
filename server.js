c00bf3f2b57a8e95cac24d67aa292545081e0064
HUNGNM130 <manhhungnguyen035@gmail.com>
2026-05-19 00:10:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
-console.log("HOST:", process.env.DB_HOST);
-console.log("USER:", process.env.DB_USER);
-console.log("PORT:", process.env.DB_PORT);
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
 const express = require("express");
 const mysql = require("mysql2");
 const multer = require("multer");
